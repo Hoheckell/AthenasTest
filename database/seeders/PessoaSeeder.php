9d0e7c3bdfc9d0f8b67d894ecf0a23429e2a8fc7
hoheckell <hoheckell.info@gmail.com>
2021-05-23 15:35:31 -0300

    ajuste categoria codigo 23_05

--- database/seeders/PessoaSeeder.php
+++ database/seeders/PessoaSeeder.php
@@ -18,17 +18,17 @@ class PessoaSeeder extends Seeder
             [
                 'nome'=>'Jorge da Silva',
                 'email'=>'jorge@terra.com.br',
-                'categoria_id'=>1
+                'categoria_codigo'=>1
             ],
             [
                 'nome'=>'Flavia Monteiro',
                 'email'=>'flavia@globo.com',
-                'categoria_id'=>2
+                'categoria_codigo'=>2
             ],
             [
                 'nome'=>'Marcos Frota Ribeiro',
                 'email'=>'ribeiro@gmail.com',
-                'categoria_id'=>2
+                'categoria_codigo'=>2
             ],
             [
                 'nome'=>'Raphael Souza Santos',
@@ -38,52 +38,52 @@ class PessoaSeeder extends Seeder
             [
                 'nome'=>'Pedro Paulo Mota',
                 'email'=>'ppmota@gmail.com',
-                'categoria_id'=>1
+                'categoria_codigo'=>1
             ],
             [
                 'nome'=>'Winder Carvalho da Silva',
                 'email'=>'winder@hotmail.com',
-                'categoria_id'=>3
+                'categoria_codigo'=>3
             ],
             [
                 'nome'=>'Maria da Penha Albuquerque',
                 'email'=>'mpa@hotmail.com',
-                'categoria_id'=>3
+                'categoria_codigo'=>3
             ],
             [
                 'nome'=>'Rafael Garcia Souza',
                 'email'=>'rgsouza@hotmail.com',
-                'categoria_id'=>3
+                'categoria_codigo'=>3
             ],
             [
                 'nome'=>'Tabata Costa',
                 'email'=>'tabata_costa@gmail.com',
-                'categoria_id'=>2
+                'categoria_codigo'=>2
             ],
             [
                 'nome'=>'Ronil Camarote',
                 'email'=>'camarote@terra.com.br',
-                'categoria_id'=>1
+                'categoria_codigo'=>1
             ],
             [
                 'nome'=>'Joaquim Barbosa',
                 'email'=>'barbosa@globo.com',
-                'categoria_id'=>1
+                'categoria_codigo'=>1
             ],
             [
                 'nome'=>'Carla Zamborlini',
                 'email'=>'zamborlini@terra.com.br',
-                'categoria_id'=>3
+                'categoria_codigo'=>3
             ],
             [
                 'nome'=>'João Paulo Vieira',
                 'email'=>'jpvieria@gmail.com',
-                'categoria_id'=>1
+                'categoria_codigo'=>1
             ],
             [
                 'nome'=>'Eveline Maria Alcantra',
                 'email'=>'ev_alcantra@gmail.com',
-                'categoria_id'=>2
+                'categoria_codigo'=>2
             ],
 
         ];
